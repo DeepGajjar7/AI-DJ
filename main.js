833423b95f7aae6548847f4e9f1911c5cc526d1e
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-06-15 11:10:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var song="";
-
+var leftwristx=0;
+var leftwristy=0;
+var rightwristx=0;
+var rightwristy=0;
 
 function preload(){
     song=loadSound("music.mp3");
@@ -10,6 +13,8 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on('pose',gotposes)
 }
 
 function draw(){
@@ -18,4 +23,26 @@ function draw(){
 
 function play() {
     song.play();
+    song.setVolume(1)
+    song.rate(1)
 }
+
+function modelloaded(){
+    console.log("model is loaded")
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        rightwristx=results[0].pose.righttWrist.x;
+        rightwristy=results[0].pose.righttWrist.y;
+    }
+}
+
+
+
+
+
+
+
